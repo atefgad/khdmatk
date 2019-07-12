fbcde4e60ce0833c2ca0ec2c1c0625bb1952acb8
atef gad <31800681+atefgad@users.noreply.github.com>
2019-07-13 00:57:51 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,43 +17,6 @@ $(document).ready(function () {
         contentType: "html"
     })
 
-    // Author Books
-    function AuthorBooks() {
-        if ($('.books-row').length) {
-            $('.books-row').owlCarousel({
-                margin: 5,
-                responsive: {
-                    0: {
-                        items: 1
-                    },
-                    600: {
-                        items: 1
-                    },
-                    1000: {
-                        items: 2
-                    }
-                }
-            })
-        };
-    };
-    AuthorBooks();
-
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 3
-            },
-            1000: {
-                items: 5
-            }
-        }
-    })
 
     var headHeight = $('.navbar').innerHeight();
     $('.main-tex').css('paddingTop', headHeight * 2);
